874ee96dfc9132e5794cc649fa7ce314722735a6
Steven Lozano <stevenlozano17@gmail.com>
2023-12-29 18:05:42 -0500

    Test

--- src/app/api/rick-and-morty/getManyCharacters.ts
+++ src/app/api/rick-and-morty/getManyCharacters.ts
@@ -3,9 +3,12 @@ import { Character } from "./character";
 
 const API_BASE_URL = "https://rickandmortyapi.com/api/character";
 
-export async function getManyCharacters(ids: number[] | string[]) {
-  console.log(ids);
-  
+// TODO: llevar estas configuraciones a un archivo config del juego. Hacer un archivo config para cada juego
+const TOTAL_CHARACTERS_API = 826;
+const TOTAL_CHARACTERS_BOARD = 10; // 4x5 board
+
+export async function getManyCharacters() {
+  const ids: number[] | string[] = generateCharactersIds();
   const idsQuery: string = ids.join(",");
   const res = await fetch(`${API_BASE_URL}/${idsQuery}`);
   if (!res.ok) {
@@ -20,3 +23,20 @@ export async function getManyCharacters(ids: number[] | string[]) {
 
   return character;
 }
+
+const generateCharactersIds = () => {
+  const charactersIds: number[] = [];
+  let i = 0;
+
+  while (i < TOTAL_CHARACTERS_BOARD) {
+    const randomId = Math.floor(
+      Math.random() * (TOTAL_CHARACTERS_API - 1 + 1) + 1
+    );
+    // Prevent repeated ids
+    if (!charactersIds.includes(randomId)) {
+      charactersIds.push(randomId);
+      i++;
+    }
+  }
+  return charactersIds;
+};
--- src/app/memo-test/page.tsx
+++ src/app/memo-test/page.tsx
@@ -2,30 +2,13 @@ import { getManyCharacters } from "../api/rick-and-morty/getManyCharacters";
 import MemoTestBoard from "./_components/memo-test-board";
 import { CharacterMemoTest } from "../api/rick-and-morty/character";
 
-const TOTAL_CHARACTERS_API = 826;
+// TODO: llevar estas configuraciones a un archivo config del juego. Hacer un archivo config para cada juego
 const TOTAL_CHARACTERS_BOARD = 10; // 4x5 board
 
 export default async function Page() {
-  const generateCharactersIds = () => {
-    const charactersIds: number[] = [];
-    let i = 0;
 
-    while (i < TOTAL_CHARACTERS_BOARD) {
-      const randomId = Math.floor(
-        Math.random() * (TOTAL_CHARACTERS_API - 1 + 1) + 1
-      );
-      // Prevent repeated ids
-      if (!charactersIds.includes(randomId)) {
-        charactersIds.push(randomId);
-        i++;
-      }
-    }
-    return charactersIds;
-  };
 
-  const characters: CharacterMemoTest[] = await getManyCharacters(
-    generateCharactersIds()
-  );
+  const characters: CharacterMemoTest[] = await getManyCharacters();
 
   return (
     <MemoTestBoard
