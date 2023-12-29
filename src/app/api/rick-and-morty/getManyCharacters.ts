0795df3f52258426ced9e2bf79cf996c3533bf85
Steven Lozano <stevenlozano17@gmail.com>
2023-12-29 17:54:35 -0500

    Test

--- src/app/api/rick-and-morty/getManyCharacters.ts
+++ src/app/api/rick-and-morty/getManyCharacters.ts
@@ -4,6 +4,8 @@ import { Character } from "./character";
 const API_BASE_URL = "https://rickandmortyapi.com/api/character";
 
 export async function getManyCharacters(ids: number[] | string[]) {
+  console.log(ids);
+  
   const idsQuery: string = ids.join(",");
   const res = await fetch(`${API_BASE_URL}/${idsQuery}`);
   if (!res.ok) {
