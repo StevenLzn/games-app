6d04411862a5433a3bc3e3960547046cff0429e7
Steven Lozano <stevenlozano17@gmail.com>
2023-12-30 18:17:15 -0500

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import GameSearchInput from "./components/game-search-input";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <GameSearchInput />
         {children}
+        <Analytics />
       </body>
     </html>
   );
